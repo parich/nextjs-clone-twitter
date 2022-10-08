16f059e668f6cdf8c6cd5c2891e057d037587742
parich <parich.suriya@gmail.com>
2022-10-08 20:12:00 +0700

    Create Sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import React from 'react';
+import SidebarMenuItem from './SidebarMenuItem';
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  EllipsisHorizontalCircleIcon,
+  EllipsisHorizontalIcon,
+  HashtagIcon,
+  HomeIcon,
+  InboxIcon,
+  UserIcon,
+} from '@heroicons/react/24/solid';
+
+export default function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      {/*Logo*/}
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+        <Image
+          width="50"
+          height="50"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+          alt="twitter logo"
+        ></Image>
+      </div>
+      {/*Menu*/}
+      <div className='mt-4 mb-2.5 xl:items-start'>
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+      </div>
+      {/*Botton*/}
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+        Tweet
+      </button>
+      {/*Mini profile*/}
+
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+        <img
+          alt="user-img"
+          src="https://arit.rmu.ac.th/images/img_personnel_new/%E0%B8%99%E0%B8%B2%E0%B8%A2%E0%B8%9B%E0%B8%A3%E0%B8%B4%E0%B8%8A%E0%B8%93%E0%B9%8C%20%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B4%E0%B8%A2%E0%B8%B0.png"
+          className="h-10 w-10 rounded-full xl:mr-2"
+        />
+
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">{'Parich suriya'}</h4>
+          <p className="text-gray-500">@{'Parich suriya'}</p>
+        </div>
+        <EllipsisHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+      </div>
+    </div>
+  );
+}
