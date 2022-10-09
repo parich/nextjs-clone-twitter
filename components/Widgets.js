3a8486c1e3c747d53495a1d62d40979c0de5fc46
parich <parich.suriya@gmail.com>
2022-10-09 20:15:07 +0700

    Create the widgets component and the search bar

---
+++ components/Widgets.js
@@ -0,0 +1,22 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+export default function Widgets() {
+  return (
+    <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+      <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
+        <div className="flex items-center p-3 rounded-full bg-red-300 relative">
+          <MagnifyingGlassIcon className="h-5 z-50 text-gray-500" />
+          <input
+            className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100 "
+            type="text"
+            placeholder="Search Twitter"
+          />
+        </div>
+      </div>
+
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">Whats happening</h4>
+      </div>
+    </div>
+  );
+}
