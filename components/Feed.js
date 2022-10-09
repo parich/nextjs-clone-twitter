80309eaecc25b36b082c2fb951715a198231ea44
parich <parich.suriya@gmail.com>
2022-10-09 19:30:31 +0700

    Add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,32 @@
 import { SparklesIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 import Input from './Input';
+import Post from './Post';
 
 export default function Feed() {
+  const posts = [
+    {
+      id: 1,
+      name: 'parich suriya',
+      username: 'parichle',
+      userImg:
+        'https://arit.rmu.ac.th/images/img_personnel_new/%E0%B8%99%E0%B8%B2%E0%B8%A2%E0%B8%9B%E0%B8%A3%E0%B8%B4%E0%B8%8A%E0%B8%93%E0%B9%8C%20%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B4%E0%B8%A2%E0%B8%B0.png',
+      img: 'https://images.unsplash.com/photo-1665243066869-1f27e948de5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+      text: 'nice view',
+      timestamp: '2 hours age',
+    },
+    {
+      id: 2,
+      name: 'suriya',
+      username: 'parichle',
+      userImg:
+        'https://arit.rmu.ac.th/images/img_personnel_new/%E0%B8%99%E0%B8%B2%E0%B8%A2%E0%B8%9B%E0%B8%A3%E0%B8%B4%E0%B8%8A%E0%B8%93%E0%B9%8C%20%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B4%E0%B8%A2%E0%B8%B0.png',
+      img: 'https://images.unsplash.com/photo-1664575601711-67110e027b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60',
+      text: 'nice view',
+      timestamp: '2 hours age',
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       {/*header*/}
@@ -16,7 +40,11 @@ export default function Feed() {
       {/*Input*/}
       <Input />
 
-      
+      {/*Posts*/}
+
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
