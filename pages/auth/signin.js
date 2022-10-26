cf07109cefa224943d740d68892991bb27daa496
parich suriya <parich.suriya@gmail.com>
2022-10-26 16:28:08 +0700

    authen faceboook

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,31 +3,33 @@ import { getProviders, signIn } from 'next-auth/react';
 export default function siginin({ providers }) {
   console.log(providers);
   return (
-    <div className="flex justify-center mt-20 space-x-4">
+    <div className="flex items-center justify-center mt-20 space-x-4">
       <img
         src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-01/ch12findphone.png.twimg.1920.png"
         alt="twitter image inside a phone"
         className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
       />
-      <div className="">
+
+      <div className="flex flex-col items-center justify-center">
+        <img
+          className="w-36 object-cover"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+          alt="twitter logo"
+        />
+        <p className="text-center text-sm italic my-5">
+          This app is created for learning purposes
+        </p>
+
         {Object.values(providers).map((provider) => (
           // eslint-disable-next-line react/jsx-key
-          <div className="flex flex-col items-center" key={provider.name}>
-            <img
-              className="w-36 object-cover"
-              src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-              alt="twitter logo"
-            />
-            <p className="text-center text-sm italic my-10">
-              This app is created for learning purposes
-            </p>
-            <button
-              onClick={() => signIn(provider.id, { callbackUrl: '/' })}
-              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
-            >
-              Sign in with {provider.name}
-            </button>
-          </div>
+
+          <button
+            key={provider.name}
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            className="bg-red-400 text-white hover:bg-red-500 rounded-lg p-3 my-5 w-60"
+          >
+            Sign in with {provider.name}
+          </button>
         ))}
       </div>
     </div>
